f95947a8660cc0330f964315993c73c3fe91ba47
Ionut Pausan <ionut_pausan93@yahoo.com>
2017-12-11 20:22:37 +0200

    log in + helpers/password

--- backEnd/control/usersC.php
+++ backEnd/control/usersC.php
@@ -1,6 +1,6 @@
 <?php
 require "model/usersModel.php";
-
+require "../helpers/password.php";
 class chatC
 {
 	private $usersModel;
@@ -8,6 +8,71 @@ class chatC
 		$this->usersModel = new usersModel();
 	}
    
+	private function logIn() {
+		function test_input($data) {
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			return $data;
+		  }
+
+		if($_SERVER["REQUEST_METHOD"]=="POST"){
+			$error=[];
+			$user = $_POST["userName"];
+			$email = $_POST["email"];
+			$password =$_POST["password"];
+			$userPattern ="/^[a-zA-Z0-9,',.,_]";
+			preg_match_all($userPattern,$user,$userMatch);	
+			// $passPattern=
+			preg_match_all($passPattern,$password,);
+			
+			//validate user name
+			if (empty($user) {
+				array_push($error,"User name was not inserted.");
+			  } else if(!empty($this->usersModel->checkUser($user)){
+				array_push($error,"This user already exists!");
+			  }else if($user == $userMatch[0][0]) {
+				$user = test_input($user);
+			  }
+			
+			  //validate email
+			if(empty($email)) {
+				array_push($error,"Please enter your email, ");
+			  }else if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+				 array_push($err,"Invalid email, ");
+			  }else if(!empty($this->usersModel->checkEmail($email))){
+				 array_push($error,"Ths email already exists!");
+			  }else{
+				  $email = test_input($user); 
+			  }  
+			  //Validate password
+			if(empty($password)){
+				array_push($error,"Please enter your password, ");
+			}else if(strlen($password)<6){
+				array_push($error,"Your password must be greater than 6 chars");
+			}else if(valid_pass($password) == false){
+				array_push($error,"Not valid password.");
+			}else if(valid_pass($password)){
+				$hased_password = valid_pass($password);
+				if($hashed_password !== $this->userModel->checkPassword($password)){
+					array_push($error,"Your password is incorect");
+				}
+			}
+		 //Final validation-setting session for a specific user
+			if(empty($error)){
+				$result = $this->usersModel->selectItem($_POST);
+				if ($result === false) {array_push($err,"Invalid Log In"); }
+				if (empty($error)) {
+					$_SESSION["id"]=$result["id"];
+					$myAccount = $this->usersModel->selectById($_SESSION["id"]);
+					$_SESSION["role"]= $result["role"];
+					$_SESSION['LAST_ACTIVITY'] = time();
+					return $_SESSION["role"];
+				}else{sleep(2);return $err;}
+			}
+		}
+	}
+}
 
 
 }?>
---
+++ backEnd/helpers/password.php
@@ -0,0 +1,25 @@
+<?php
+function passCrypt($pass){
+    $salt ='$5$rounds=5000$usesomes2343ilasslystgforlt$'; 
+    return $hashed_password = crypt($pass,$salt);
+}
+
+function valid_pass($pass) {
+    $r1='/[A-Z]/';  //Uppercase
+    $r2='/[a-z]/';  //lowercase
+    $r3='/[!@#$%^&*()\-_=+{};:,<.>]/';  // whatever you mean by 'special char'
+    $r4='/[0-9]/';  //numbers
+ 
+    if(preg_match_all($r1,$candidate, $o)<2) return FALSE;
+ 
+    if(preg_match_all($r2,$candidate, $o)<2) return FALSE;
+ 
+    if(preg_match_all($r3,$candidate, $o)<2) return FALSE;
+ 
+    if(preg_match_all($r4,$candidate, $o)<2) return FALSE;
+ 
+    if(strlen($candidate)<8) return FALSE;
+ 
+    return passCrypt($pass);
+ }
+?>
