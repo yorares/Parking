5fc8435876df1331eb1fe5e08612c20eda90f43a
yorares <name_no_clue@yahoo.com>
2017-12-13 11:37:12 +0200

    aswd
    dwad

--- db/parking (2).sql
+++ db/parking (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 10, 2017 at 06:00 PM
+-- Generation Time: Dec 13, 2017 at 09:36 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -108,16 +108,27 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(200) NOT NULL,
   `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_login` timestamp NULL DEFAULT NULL,
-  `user_status` enum('online','offline') NOT NULL DEFAULT 'online',
-  `admin` enum('user','admin','quest') NOT NULL,
+  `user_status` enum('online','offline') NOT NULL DEFAULT 'offline',
+  `admin` enum('user','admin','quest') NOT NULL DEFAULT 'user',
   `password` varchar(500) NOT NULL,
   `birth_date` date NOT NULL,
   `phone` int(11) NOT NULL,
-  `picture_link` varchar(200) NOT NULL,
+  `picture_link` varchar(200) DEFAULT NULL,
+  `user_ip` varchar(30) DEFAULT NULL,
+  `active` enum('banned','unbanned') NOT NULL DEFAULT 'unbanned',
+  `ban_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_name` (`user_name`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `user_name`, `email`, `creation_date`, `last_login`, `user_status`, `admin`, `password`, `birth_date`, `phone`, `picture_link`, `user_ip`, `active`, `ban_time`) VALUES
+(2, 'Rares', 'Puscas', 'yorares30', 'rares@gmail.com', '2017-12-12 11:16:25', NULL, 'online', 'user', '$5$rounds=5000$usesomes2343ilas$iZeZPEhQmOY3Rb/ykPPTijt/7UNqmw1JD1mgXWjXwnD', '2000-10-11', 741058260, NULL, '::1', 'unbanned', NULL),
+(3, 'Rares', 'Puscas', 'yorareadwads30', 'raradawdes@gmail.com', '2017-12-12 11:20:27', NULL, 'offline', 'user', '$5$rounds=5000$usesomes2343ilas$iZeZPEhQmOY3Rb/ykPPTijt/7UNqmw1JD1mgXWjXwnD', '2000-10-11', 741058260, NULL, '::1', 'unbanned', NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
