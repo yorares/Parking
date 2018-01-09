29b98b5380abf445e465cbd84b42d44fefede960
yorares <name_no_clue@yahoo.com>
2018-01-09 17:56:50 +0200

    sass si usersConnect

--- frontEnd/js/backEnd/userConnect.js
+++ frontEnd/js/backEnd/userConnect.js
@@ -1,3 +1,35 @@
 var userConnect = (function ($) {
+    var BASE_URL = 'https://web-10-yorares.c9users.io/PHP Backend/';
+    var endpoints = {
+        contact: BASE_URL + 'contact',
+        search: BASE_URL + 'search',
+        getComments: BASE_URL + 'Comments/list',
+        addComment: BASE_URL + 'Comments/add',
+        getAll: BASE_URL + 'Articles/list',
+        create: BASE_URL + 'Articles/add',
+        deleteOne: BASE_URL + 'Articles/delete',
+        update: BASE_URL + 'Articles/edit',
+        logIn: BASE_URL + 'logIn',
+        signUp: BASE_URL + 'signUp',
+        logOut: BASE_URL + 'logOut',
+        myAccount: BASE_URL + 'MyAccount'
+    };
 
+    var make_call = function (params, callback) {
+        $.ajax({
+            url: params.url,
+            method: params.method,
+            data: params.data || null,
+            success: function (result) {
+                var resultParsat = JSON.parse(result);
+                callback(null, resultParsat);
+            },
+            error: function (XHR, status, error) {
+                callback(error);
+            },
+            complete: function (XHR, status) {
+
+            }
+        });
+    };
 })($);
