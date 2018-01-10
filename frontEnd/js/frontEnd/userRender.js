5d32f55f37162d3f03555ab83ee54df9330ff9b2
yorares <name_no_clue@yahoo.com>
2018-01-10 19:16:08 +0200

    log in render

--- frontEnd/js/frontEnd/userRender.js
+++ frontEnd/js/frontEnd/userRender.js
@@ -1,3 +1,24 @@
 var userRender = (function ($) {
+    var logInRender = function () {
+        let html = "<div class='logInDiv'>";
+        html += "<input type='text' class='input logIn' placeholder='User Name or Email' />";
+        html += "<input type='password' class='input logIn' name='password' placeholder='password' />";
+        html += "<input type='button' class='button logInBtn' value='Log In' />";
+        html += "</div>";
+        return html;
+    }
+
+
+    return {
+        htmlLogIn: function () {
+            $('.output').html(logInRender());
+            $('input[name=password]').keypress(function (e) {
+                if (e.keyCode == 13) {
+                    $('.logInBtn').click();
+                }
+            });
+        }
+
+    }
 
 })($);
