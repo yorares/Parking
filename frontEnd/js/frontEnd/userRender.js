67aa4c71c10f4e97c413540fd14334f1b6b20181
Ionut Pausan <ionut_pausan93@yahoo.com>
2018-01-10 19:33:57 +0200

    user reder conflict resolved

--- frontEnd/js/frontEnd/userRender.js
+++ frontEnd/js/frontEnd/userRender.js
@@ -1,28 +1,27 @@
-<<<<<<< HEAD
 var userRender = (function($) {
-=======
-var userRender = (function ($) {
-    var logInRender = function () {
-        let html = "<div class='logInDiv'>";
-        html += "<input type='text' class='input logIn' placeholder='User Name or Email' /><br>";
-        html += "<input type='password' class='input logIn' name='password' placeholder='password' /><br>";
-        html += "<input type='button' class='button logInBtn' value='Log In' />";
-        html += "</div>";
-        return html;
-    }
 
-
-    return {
-        htmlLogIn: function () {
-            $('.output').html(logInRender());
-            $('input[name=password]').keypress(function (e) {
-                if (e.keyCode == 13) {
-                    $('.logInBtn').click();
+            var userRender = (function($) {
+                var logInRender = function() {
+                    let html = "<div class='logInDiv'>";
+                    html += "<input type='text' class='input logIn' placeholder='User Name or Email' /><br>";
+                    html += "<input type='password' class='input logIn' name='password' placeholder='password' /><br>";
+                    html += "<input type='button' class='button logInBtn' value='Log In' />";
+                    html += "</div>";
+                    return html;
                 }
-            });
-        }
 
-    }
->>>>>>> 5d32f55f37162d3f03555ab83ee54df9330ff9b2
 
-})($);
+                return {
+                    htmlLogIn: function() {
+                        $('.output').html(logInRender());
+                        $('input[name=password]').keypress(function(e) {
+                            if (e.keyCode == 13) {
+                                $('.logInBtn').click();
+                            }
+                        });
+                    }
+
+                }
+
+
+            })($);
